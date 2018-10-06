8d618781d427c6a3e65857f027121088784fc771
ziad harb <zfharb@yahoo.com>
2018-10-06 16:34:26 -0400

    modified files

--- src/components/picture.js
+++ src/components/picture.js
@@ -15,11 +15,11 @@ const Picture = (props) => {
 
       if (props.showProfile) {
       pictureSection =  <div className="picture-show-section-background" >
-                          <div className="picture-show-image"/>   
+                          <img className="picture-show-image"/>   
                         </div>;
       } else {
       pictureSection =  <div className="picture-hide-section-background" >
-                          <div className="picture-hide-image"/>    
+                          <img className="picture-hide-image"/>    
                         </div>;
                       
       }
