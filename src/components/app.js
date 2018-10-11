403c00bf094db6cc2180a6dca0826871e4879d42
Ziad Harb <zharb@ll28b2bd143926.int.pithoslabs.com>
2018-10-11 12:00:27 -0400

    modified files

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Home from '../containers/home';
@@ -6,17 +6,18 @@ import About from '../containers/about';
 
 import './app.css'
 
-const App = (props) => {
-
+class App extends Component {
+	render() {
     return (
+
       <Router>
-        <div>
+    	<div>
           <Route exact path="/" Component={Home} />
           <Route path="/about" Component={About} />
-          
-        </div>
+        </div>          
       </Router>
-    )
+    );
+}
 }
 
 export default App;
--- src/containers/about.js
+++ src/containers/about.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import { Link} from 'react-router-dom';
-import {connect} from 'react-redux'
 
 
 import './about.css'
@@ -8,27 +6,19 @@ import './about.css'
 class About extends Component {
 
   constructor(props) {
-    super(props);
-    
-    
+    super(props);    
   }
-
-  
-
 
   render() {
   
     return (
       <div>
-    about page
+        about page
       </div>
     )
   }
 }
 
 
-function mapStateToProps(state) {
-  return {output: state.output};
-}
 
-export default connect(mapStateToProps,null)(About);
+export default About;
